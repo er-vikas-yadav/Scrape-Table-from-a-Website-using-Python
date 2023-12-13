e60a5174c8d6500c30351dd27929fee68629d7b9
Vikas Yadav <153706033+er-vikas-yadav@users.noreply.github.com>
2023-12-13 22:05:19 +0530

    Add files via upload

---
+++ table_scrap.py
@@ -0,0 +1,12 @@
+import urllib.request
+import pandas as pd
+url = "https://en.wikipedia.org/wiki/Programming_languages_used_in_most_popular_websites"
+
+with urllib.request.urlopen(url) as i:
+    html = i.read()
+    
+data = pd.read_html(html)[0]
+print(data.head())
+
+#If you want to save this data in a CSV file, below is how you can save it:
+data.to_csv("programming.csv")
